351e0f0d809869ce952d177d7f1c3436edd65b38
Andrii2024 <krovlya2005@gmail.com>
2024-02-22 15:58:51 +0200

    up

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -7,67 +7,67 @@ import {
   StyledLabel,
 } from './App.styled';
 
-export const ContactForm = () => {
+export const ContactForm = ({ onAddContact, contacts }) => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
-};
 
-const handleInputChange = e => {
-  const { name, value } = e.target;
-  // this.setState({ [name]: value });
-  setName(value);
-};
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
+  };
 
-const handleSubmit = e => {
-  e.preventDefault();
-  const { name, number } = this.state;
-  const { contacts } = this.props;
+  const handleSubmit = e => {
+    e.preventDefault();
 
-  const isNameExists = contacts.some(
-    contact => contact.name.toLowerCase() === name.toLowerCase()
-  );
+    const isNameExists = contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
 
-  if (isNameExists) {
-    alert(`${name} is already in contacts`);
-    // this.setState({ name: '', number: '' });
+    if (isNameExists) {
+      alert(`${name} is already in contacts`);
+
+      setName('');
+      setNumber('');
+      return;
+    }
+
+    onAddContact({
+      id: nanoid(),
+      name: name.trim(),
+      number: number.trim(),
+    });
+
     setName('');
     setNumber('');
-    return;
-  }
+  };
 
-  this.props.onAddContact({
-    id: nanoid(),
-    name: name.trim(),
-    number: number.trim(),
-  });
-
-  // this.setState({ name: '', number: '' });
-  setName('');
-  setNumber('');
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledLabel>
+        Name:
+        <StyledInput
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleInputChange}
+          required
+        />
+      </StyledLabel>
+      <StyledLabel>
+        Number:
+        <StyledInput
+          type="text"
+          name="number"
+          value={number}
+          onChange={handleInputChange}
+          required
+        />
+      </StyledLabel>
+      <StyledButton type="submit">Add Contact</StyledButton>
+    </StyledForm>
+  );
 };
-
-return (
-  <StyledForm onSubmit={handleSubmit}>
-    <StyledLabel>
-      Name:
-      <StyledInput
-        type="text"
-        name="name"
-        value={name}
-        onChange={handleInputChange}
-        required
-      />
-    </StyledLabel>
-    <StyledLabel>
-      Number:
-      <StyledInput
-        type="text"
-        name="number"
-        value={number}
-        onChange={handleInputChange}
-        required
-      />
-    </StyledLabel>
-    <StyledButton type="submit">Add Contact</StyledButton>
-  </StyledForm>
-);
